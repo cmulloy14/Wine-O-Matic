8a9d6731484a92164a2af2d679c050a5bf46959c
Charles Mulloy <charles.mulloy@etrade.com>
2018-12-04 22:28:29 -0600

    Initial Commit

---
+++ Wine-O-Matic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wine-O-Matic
+//
+//  Created by Mulloy, Charles on 12/4/18.
+//  Copyright © 2018 Pat Mulloy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Wine-O-MaticTests/Wine_O_MaticTests.swift
@@ -0,0 +1,34 @@
+//
+//  Wine_O_MaticTests.swift
+//  Wine-O-MaticTests
+//
+//  Created by Mulloy, Charles on 12/4/18.
+//  Copyright © 2018 Pat Mulloy. All rights reserved.
+//
+
+import XCTest
+@testable import Wine_O_Matic
+
+class Wine_O_MaticTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
